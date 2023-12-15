b59f55ce9ddc31488461d78b6b8663c7602b69ac
ehmedosmanov <ehmedosmanov@gmail.com>
2023-12-15 13:45:14 +0400

    Hero Slider Completed

--- client/src/components/HeroComponents/index.tsx
+++ client/src/components/HeroComponents/index.tsx
@@ -9,10 +9,10 @@ const Hero = (props: Props) => {
         <section id='hero' className='py-[40px]'>
             <div className="wrapper">
                 <div className="hero-content grid grid-cols-12 gap-4">
-                    <div className="categories col-span-12 md:col-span-6 lg:col-span-3">
+                    <div className="categories col-span-12  lg:col-span-3">
                         <SidebarCategories />
                     </div>
-                    <div className="hero-slider col-span-12 md:col-span-6 lg:col-span-9">
+                    <div className="hero-slider col-span-12  lg:col-span-9">
                         <HeroSlider />
                     </div>
                 </div>
